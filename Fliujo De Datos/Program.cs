8fc2e793480ce384ac7153814dd746907a09d925
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-07 13:10:41 -0600

    new streamReader

---
+++ Fliujo De Datos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Flujo_De_Datos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream("Prueba.txt", FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (StreamWriter streamWriter= new StreamWriter(fileStream))
+                {
+                    streamWriter.WriteLine("Hello everyone I'm Dickson");
+                }
+            }
+
+
+
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
